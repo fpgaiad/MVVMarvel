d045ee36ea7bac361e53d2b58b8c77bf3ccfe86c
Felipe Gaiad <fpgaiad@gmail.com>
2020-06-24 12:27:32 -0300

    Update ViewModel components

--- app/src/main/java/com/mvvmarvel/viewmodel/CharactersViewModel.kt
+++ app/src/main/java/com/mvvmarvel/viewmodel/CharactersViewModel.kt
@@ -1,6 +1,13 @@
 package com.mvvmarvel.viewmodel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import com.mvvmarvel.model.Result
+import com.mvvmarvel.repository.MarvelRepository
 
-class CharactersViewModel : ViewModel() {
+class CharactersViewModel(private val repository: MarvelRepository) : ViewModel() {
+
+    fun getCharacters(): LiveData<List<Result>> {
+        return repository.getCharacters()
+    }
 }
