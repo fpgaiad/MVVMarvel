f2947ac352b08dd8e8be792a0888169165487cb9
Felipe Gaiad <fpgaiad@gmail.com>
2020-07-01 00:11:49 -0300

    Update repositories

--- app/src/main/java/com/mvvmarvel/repository/MarvelRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.mvvmarvel.repository
-
-import androidx.lifecycle.MutableLiveData
-import com.mvvmarvel.model.Result
-
-interface MarvelRepository {
-
-    fun getCharacters(): MutableLiveData<List<Result>>
-}
